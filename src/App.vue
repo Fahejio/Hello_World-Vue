b8aa6497bc7b53d358861f696be046d43888ef4e
Fahejio <rafaelfernandes816@gmail.com>
2023-09-25 04:08:45 -0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Hello World"/>
+    <img alt="Vue logo" src="./assets/logoRe.png">
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font: italic 1.2rem "Fira Sans", serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
